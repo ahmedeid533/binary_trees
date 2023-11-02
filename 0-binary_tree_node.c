d5c2252527728db0f89ca03132236030f21811c9
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-02 11:33:41 +0200

    binary tree 0.02

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
@@ -19,11 +18,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     new_node->n = value;
     new_node->left = NULL;
     new_node->right = NULL;
-    if (parent)
-    {   if (parent->left)
-            parent->left = new_node;
-        else
-            parent->right = new_node;
-    }
-    return (new_node);
+    new_node->parent = parent;
 }
