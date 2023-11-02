481e331961497eb57a1e4efe61d5364334c455ef
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-02 18:40:27 +0200

    binary tree E.02

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
 /**
  * binary_tree_balance - calc the balance
  * @tree: A pointer to root
@@ -9,6 +8,23 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
 		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (0);
+}
 
+/**
+ * binary_tree_height - calc height
+ * @tree: A pointer to the tree
+ * Return: height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t left = 0, right = 0;
+
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((left < right) ? right : left);
+	}
 	return (0);
 }
