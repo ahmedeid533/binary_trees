ae01801e2c6570ee7017f816e4e89de4e5a3879c
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-02 18:09:09 +0200

    binary tree 4.01

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,11 +7,11 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if(!node)
+	if (!node)
 		return (0);
-	if(node->left)
+	if (node->left)
 		return (0);
-	if(node->right)
+	if (node->right)
 		return (0);
 	return (1);
 }
