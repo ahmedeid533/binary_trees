f6b0e55260d3b9c25ee0354b15d4491e4c734978
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-11-02 17:48:14 +0200

    binary tree 1.05

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,8 +10,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (!parent)
+		return (NULL);
 	new_node = binary_tree_node(parent, value);
-	if (!new_node || !parent)
+	if (!new_node)
 		return (NULL);
 
 	if (parent->left)
